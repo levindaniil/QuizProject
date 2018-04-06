a6b88330308b5b8db1535ffaed27d6f5b453ca06
User\Olga <olyakras27@gmail.com>
2018-04-06 18:19:30 +0300

    Add multipul choice answer page.

--- QuizTool.UI/MultipleAnswerPage.xaml.cs
+++ QuizTool.UI/MultipleAnswerPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuizTool.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,89 @@ namespace QuizTool.UI
     /// </summary>
     public partial class MultipleAnswerPage : Page
     {
-        public MultipleAnswerPage()
+        public MultipleAnswerPage(Question _question, List<Answer> _answers)
         {
             InitializeComponent();
+            question = _question;
+            answers = _answers;
+        }
+
+        Question question;
+        List<Answer> answers;
+        List<CheckBox> checkBoxes = new List<CheckBox>();
+
+
+        private void CreateGrid(List<Answer> answers)
+        {
+            int c = answers.Count();
+
+            for (int i = 0; i < c; i++)
+            {
+                var newRow = new RowDefinition();
+                newRow.Height = new GridLength(60);
+                gridAnswers.RowDefinitions.Add(newRow);
+            }
+
+            for (int i = 0; i < c; i++)
+            {
+                CheckBox newCheckBox = new CheckBox();
+                newCheckBox.Name = $"CheckBox{answers[i].Id}";
+                newCheckBox.IsEnabled = true;
+                newCheckBox.Content = $"  {answers[i].Text}";
+                newCheckBox.FontSize = 20;
+                newCheckBox.HorizontalContentAlignment = HorizontalAlignment.Left;
+                newCheckBox.HorizontalAlignment = HorizontalAlignment.Stretch;
+                newCheckBox.Margin = new Thickness(5);
+                CheckBox checkBox = new CheckBox();
+
+                Grid.SetRow(newCheckBox, i);
+                checkBoxes.Add(newCheckBox);
+            }
+
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            buttonSubmit.Visibility = Visibility.Hidden;
+            buttonExit.Visibility = Visibility.Visible;
+            var correctAnswers = answers.FindAll(a => a.IsCorrect == true);
+            var chosenAnswers = checkBoxes.FindAll(a => a.IsChecked == true);
+            
+            foreach (var item in checkBoxes)
+            {
+                foreach(var ans in correctAnswers)
+                {
+                    if ($"CheckBox{ans.Id}" == item.Name)
+                    {
+                        if (item.IsChecked==true)
+                        {
+                            item.Foreground = Brushes.Green;
+                            item.Content += "  - Correct";
+                            chosenAnswers.Remove(item);
+                        }
+                        else { item.Content += "  - Correct, Not Chosen"; }
+                    }
+                }
+            }
+
+            foreach (var item in  chosenAnswers)
+            {
+                item.Foreground = Brushes.Red;
+                item.Content += "  - Uncorrect";
+            }
+
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            CreateGrid(answers);
+            textBoxQuestion.Text = $" {question.Text}";
+        }
+
+        private void buttonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
